454d7d733ae01a8f03cfe86d9f6060c23569fbb9
AbigailMedina <AMEDINA2016@columbiasecondary.org>
2019-12-04 14:05:09 -0500

    created app.py version of server and updated logic Layer skeleton

--- logicLayer.py
+++ logicLayer.py
@@ -1,24 +1,28 @@
-dataLayer = DataLayer()
+from dataLayer import DataLayer
+dl = DataLayer()
 
 #TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document).
 # IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
 
+class LogicLayer:
 
-getDocs(list_of_ngrams):
-	
-	documentList = dataLayer.get(list_of_ngrams)
-	# for each document in documentList
-		#attach scores (tf and idf) to document
+	def getDocs(list_of_ngrams):
+		
+		documentList = dl.get(list_of_ngrams)
+		return documentList
+		# for each document in documentList
+			#attach scores (tf and idf) to document
 
-removeDoc(documentData):
-	
-	# for each word in document data
-		#remove document from documentList
-		#update idf score for the word
+	def removeDoc(documentData):
+		pass
+		# for each word in document data
+			#remove document from documentList
+			#update idf score for the word
 
-addDoc(documentData):
+	def addDoc(documentData):
+		pass
 
-	# for each word in document data
-		#add document from documentList
-		#calculate tf
-		#update idf score for the word
+		# for each word in document data
+			#add document from documentList
+			#calculate tf
+			#update idf score for the word
