454af8fe846a3dddb88815903aacb68a2529e805
AbigailMedina <AMEDINA2016@columbiasecondary.org>
2019-12-08 16:11:14 -0500

    debugging

--- logicLayer.py
+++ logicLayer.py
@@ -5,35 +5,36 @@ dl = DataLayer()
 #TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document).
 # IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
 
-class LogicLayer:
-	
-	#converts input to output format for adding word
-	def changeFormat(documentData,word):
-		#calculate tf and idf
-		tf = double(word["Count"]) / double(documentData["Words"]["NumWords"])
-		numDocs = len(getDocs([word["Text"]])[0]) + 1
-		idf = np.log(10 / numDocs)
-                #TODO 10 is totalDocs hardcoded till we get that from dds
-		#add word to list in proper format
-		return {
-			"text": word["Text"],
-			"documentId": documentData["DocumentID"],
-			"document":
-			{
-				"tf": tf,
-				"idf": idf,
-				"occurrences": word["Occurences"]
-			}
+#converts input to output format for adding word
+def changeFormat(self,documentData,word):
+	#calculate tf and idf
+	tf = double(word["Count"]) / double(documentData["Words"]["NumWords"])
+	numDocs = len(getDocs([word["Text"]])[0]) + 1
+	idf = np.log(10 / numDocs)
+            #TODO 10 is totalDocs hardcoded till we get that from dds
+	#add word to list in proper format
+	return {
+		"text": word["Text"],
+		"documentId": documentData["DocumentID"],
+		"document":
+		{
+			"tf": tf,
+			"idf": idf,
+			"occurrences": word["Occurences"]
 		}
+	}
 
-	def getDocs(list_of_ngrams):
+
+class LogicLayer:
+
+	def getDocs(self,ist_of_ngrams):
 
 		documentList = dl.get(list_of_ngrams)
 		return documentList
 		# for each document in documentList
 			#attach scores (tf and idf) to document
 
-	def removeDoc(documentData):
+	def removeDoc(self,documentData):
 		#get list of words
 		words = []
 		for word in documentData["Words"]["WordCounts"]:
@@ -44,7 +45,7 @@ class LogicLayer:
 			#remove document from documentList
 			#update idf score for the word
 	
-	def addDoc(documentData):
+	def addDoc(self,documentData):
 		words = []
 		for word in documentData["Words"]["WordCounts"]:
 			words.append(changeFormat(documentData,word))
