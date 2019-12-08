e6494c8f0198f8ffac48808a437347f9e206a9ff
AbigailMedina <AMEDINA2016@columbiasecondary.org>
2019-12-08 18:13:26 -0500

    getDocs implementation

--- logicLayer.py
+++ logicLayer.py
@@ -22,31 +22,66 @@ def changeFormatAdd(documentData,word):
 		}
 	}
 
+''' returns:
+[
+	{
+	“_id”:“Cow”,
+	“value”:{
+		“documents”:
+			{
+				“docid1”:
+					{
+						“idf”:0.6931471805599453,
+						“occurrences”:[10.0,11.0,12.0],
+						“tf”:0.25,
+						“ts”:1575842072.742201}
+					},
+				"docid2":
+					{
+						“idf”:0.6931471805599453,
+						“occurrences”:[10.0,11.0,12.0],
+						“tf”:0.25,
+						“ts”:1575842072.742201}
+					}
+			}
+	}
+]
 
+
+{
+   “NGRAM1”:
+    {
+       “DocumentId1”:{documentData},
+       “DocumentId2”: {documentData},...
+    },
+    “NGRAM2”:...
+}
+'''
 class LogicLayer:
+	def calculateIDF(word, numDocsForWord):
+		#ask dds for num documents
+		totalNumOfDocuments = 10
+		# IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
+		return np.log(totalNumOfDocuments / numDocsForWord)
 
-	def getDocs(self,liAddst_of_ngrams):
+	def getDocs(self,list_of_ngrams):
 		documentGet = dl.get(list_of_ngrams)
+		ret = dict()
+		for word in documentGet:
 
-		for doc in documentGet:
+			idf = calculateIDF(word['_id'],length(word['value']['documents']))
 
-
-		docs =
-
-
-
-
-		#calculate idf and tf-idf
-		numDocs = 5#len(dl.get([word["Text"]])[0]) + 1
-		#TODO 10 is totalDocs hardcoded till we get that from dds
-		idf = np.log(10 / numDocs)
-		tfidf = tf * idf
-
-
-		return docs
-		# for each document in documentList
-			#attach scores (tf and idf) to document
-
+			for doc in word['value']['documents']:
+				oneDoc = dict()
+				documentData = dict()
+				documentData['idf'] = idf
+				documentData['tf'] = doc['tf']
+				documentData['tf-idf'] = doc['tf']*idf
+				oneDoc[doc.key()] = documentData
+				ret[word['_id']].append( oneDoc )
+				#how to append to dict?
+		return ret
+		
 	def removeDoc(self,documentData):
 		#get list of words
 		words = []
