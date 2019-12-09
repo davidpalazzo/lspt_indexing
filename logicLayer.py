6770f2dd76d2cb04b4748f9070295e978879c971
Canshi Wei <canshi@rift-88.dynamic2.rpi.edu>
2019-12-08 19:12:11 -0500

    comment, formating

--- logicLayer.py
+++ logicLayer.py
@@ -1,16 +1,15 @@
 from dataLayer import DataLayer
 import numpy as np
 
-dl = DataLayer()
 
-
-# TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document).
-# IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
-#  TF-IDF(t) = TF(t) * IDF(t)
-
-# converts input to output format for adding word
 def changeFormatAdd(documentData, word):
-    # calculate tf
+    """
+    changeFormatAdd change the input the data format to the one can be accepted by database
+    :param documentData:
+    :param word: the word of the document data refer to
+    :return: the format could be accepted by database
+    """
+    # TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document).
     tf = float(word["Count"]) / float(documentData["Words"]["NumWords"])
 
     # add word to list in proper format
@@ -25,57 +24,90 @@ def changeFormatAdd(documentData, word):
     }
 
 
-''' 
-returns:
-[
-	{
-	“_id”:“Cow”,
-	“value”:{
-		“documents”:
-			{
-				“docid1”:
-					{
-						“idf”:0.6931471805599453,
-						“occurrences”:[10.0,11.0,12.0],
-						“tf”:0.25,
-						“ts”:1575842072.742201}
-					},
-				"docid2":
-					{
-						“idf”:0.6931471805599453,
-						“occurrences”:[10.0,11.0,12.0],
-						“tf”:0.25,
-						“ts”:1575842072.742201}
-					}
-			}
-	}
-]
-
-
-{
-   “NGRAM1”:
-    {
-       “DocumentId1”:{documentData},
-       “DocumentId2”: {documentData},...
-    },
-    “NGRAM2”:...
-}
-'''
+def calculateIDF(numDocsForWord):
+    """
+    calculateIDF calculate the IDF for a word
+    :param numDocsForWord: number of document associate with a specific word
+    :return: the idf calculated
+    """
+    # ask dds for num documents
+    # TODO: currently hard code the number of document as 10
+    total = 10
+    # IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
+    return np.log(total / numDocsForWord)
 
 
 class LogicLayer:
-    def calculateIDF(self, word, numDocsForWord):
-        # ask dds for num documents
-        totalNumOfDocuments = 10
-        # IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
-        return np.log(totalNumOfDocuments / numDocsForWord)
+    """
+    LogicLayer supposed to handle all the logic in component,
+    for example calculate tf, idf, tf-idf, and transforming format, etc.
+    It remain the extensibility of the system, and may involve the multiprocessing in future.
+
+    TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document).
+    IDF(t) = log_e(Total number of documents / Number of documents with term t in it).
+    TF-IDF(t) = TF(t) * IDF(t)
+    """
+
+    def __init__(self):
+        """
+        constructor generate the constant of data layer
+        """
+        self.dataLayer = DataLayer()
 
     def getDocs(self, list_of_ngrams):
-        contents = dl.get(list_of_ngrams)
+        """
+        getDocs get the list of ngrams from data layer,
+        then convert the format that needed by customer
+
+        from data layer
+        [
+            {
+            “_id”:“Cow”,
+            “value”:{
+                “documents”:
+                    {
+                        “docid1”:
+                            {
+                                “idf”:0.6931471805599453,
+                                “occurrences”:[10.0,11.0,12.0],
+                                “tf”:0.25,
+                                “ts”:1575842072.742201}
+                            },
+                        "docid2":
+                            {
+                                “idf”:0.6931471805599453,
+                                “occurrences”:[10.0,11.0,12.0],
+                                “tf”:0.25,
+                                “ts”:1575842072.742201}
+                            }
+                    }
+            }
+        ]
+
+        transformed format
+        {
+           “NGRAM1”:
+            {
+               “DocumentId1”:{documentData},
+               “DocumentId2”: {documentData},...
+            },
+            “NGRAM2”:...
+        }
+
+        :param list_of_ngrams: list of words that need to query
+        :return: the result of list of word
+        """
+
+        # get the documents from data layer
+        contents = self.dataLayer.get(list_of_ngrams)
+
+        # transform the format
         ret = dict()
+
+        # loop through documents get, calculate the idf
         for content in contents:
             text, documents = content['_id'], content["value"]["documents"]
-            idf = self.calculateIDF(text, len(documents))
+            idf = calculateIDF(len(documents))
 
             ret[text] = dict()
             for doc_id in documents:
@@ -86,18 +118,29 @@ class LogicLayer:
                 ret[text][doc_id] = documentData
 
     def removeDoc(self, documentData):
+        """
+        removeDoc remove the document given
+        :param documentData: Words associate with a list of words
+        :return: the result from data layer
+        """
         # get list of words
         words = []
         for word in documentData["Words"]["WordCounts"]:
             words.append(word["Text"])
         # delete document from words
-        return dl.delete_text(documentData["DocumentID"], words)
+        return self.dataLayer.delete_text(documentData["DocumentID"], words)
 
-    # for each word in document data
-    # remove document from documentList
-    # update idf score for the word
+        # for each word in document data
+        # remove document from documentList
+        # update idf score for the word
 
     def addDoc(self, documentData):
+        """
+        recieved document data from server layer,
+         and transforming to the format could be accpeted by data layer
+        :param documentData: documentData from server layer
+        :return: the result output from data layer
+        """
         words = []
         for word in documentData["Words"]["WordCounts"]:
             words.append(changeFormatAdd(documentData, word))
@@ -105,9 +148,9 @@ class LogicLayer:
             words.append(changeFormatAdd(documentData, bigram))
         for trigram in documentData["NGrams"]["TriGrams"]:
             words.append(changeFormatAdd(documentData, trigram))
-        return dl.put(words)
+        return self.dataLayer.put(words)
 
-    # for each word in document data
-    # add document from documentList
-    # calculate tf
-    # update idf score for the word
+        # for each word in document data
+        # add document from documentList
+        # calculate tf
+        # update idf score for the word
